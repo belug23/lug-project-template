b756d8041e837e95c1923d1d7756fd3f46b4715c
Alexandre Lessard <alexandre.lessard.ti@gmail.com>
2014-11-25 21:46:48 -0500

    initial commit

---
+++ dev_setup.sh
@@ -0,0 +1,2 @@
+export DJANGO_SETTINGS_MODULE={{ project_name }}.settings.local
+export DJANGO_APP_TEMPLATE=https://github.com/belug/lug-app-template/archive/master.zip
---
+++ project_name/settings/local.py
@@ -0,0 +1,31 @@
+from base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postresql_psycopg2',
+        'NAME':     '{{ project_name }}',
+        'USER':     '',
+        'PASSWORD': '',
+        'HOST':     '',
+        'PORT':     '',
+    }
+}
+
+########## TOOLBAR CONFIGURATION
+# See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html#explicit-setup
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+# http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
+INTERNAL_IPS = ('127.0.0.1',)
+########## END TOOLBAR CONFIGURATION
